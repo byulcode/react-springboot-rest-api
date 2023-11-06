644b064d0299c7b9418ea7eb62b4c980e26c2dc1
byulcode <bybit0408@gmail.com>
2023-11-06 22:32:59 +0900

    initial commit

---
+++ src/main/java/com/proud/proudcatorder/ProudCatOrderApplication.java
@@ -0,0 +1,13 @@
+package com.proud.proudcatorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProudCatOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProudCatOrderApplication.class, args);
+    }
+
+}
